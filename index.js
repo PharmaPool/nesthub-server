bc6fc219b92d3077f21a53a51ef28f7f67762946
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2025-02-11 13:29:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const compression = require("compression");
 const auth_route = require("./routes/authRoutes");
 const user_route = require("./routes/userRoutes");
+const member_route = require("./routes/memberRoutes");
 
 dotenv.config();
 
@@ -38,7 +39,21 @@ app.use((req, res, next) => {
 app.get("/", (req, res, next) => res.send("Hello world !!!"));
 app.use("/api/auth", auth_route);
 app.use("/api/user", user_route);
+app.use("/api/member", member_route);
 
+async function deleteAllCollections() {
+  await mongoose.connect(process.env.MONGO_URL);
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.drop();
+    console.log(`Dropped collection: ${collection.collectionName}`);
+  }
+
+  console.log("All collections deleted.");
+  await mongoose.connection.close();
+}
+// deleteAllCollections();
 // connect to database
 mongoose
   .connect(process.env.MONGO_URL)
